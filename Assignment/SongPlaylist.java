6891c565c1de87bc991118df7036a2ebb3ab4580
Harith Iqbal <hrithiqball@gmail.com>
2024-11-25 00:24:45 +0800

    feat: added assignment code

--- Assignment/SongPlaylist.java
+++ Assignment/SongPlaylist.java
@@ -1,5 +1,41 @@
 package Assignment;
 
+import java.util.LinkedList;
+
 public class SongPlaylist {
 
+  private LinkedList<String> playlist;
+
+  SongPlaylist() {
+    playlist = new LinkedList<String>();
+
+    playlist.add("Song 1");
+    playlist.add("Song 2");
+    playlist.add("Song 3");
+    playlist.add("Song 4");
+    playlist.add("Song 5");
+    playlist.add("Song 6");
+    playlist.add("Song 7");
+    playlist.add("Song 8");
+    playlist.add("Song 9");
+    playlist.add("Song 10");
+  }
+
+  private void displayPlaylist() {
+    System.out.println("Playlist: ");
+
+    for (int i = 0; i < playlist.size(); i++) {
+      System.out.println(playlist.get(i));
+    }
+  }
+
+  public static void main(String[] args) {
+    SongPlaylist playlist = new SongPlaylist();
+
+    for (String song : playlist.playlist) {
+      System.out.println(song);
+    }
+
+    playlist.displayPlaylist();
+  }
 }
